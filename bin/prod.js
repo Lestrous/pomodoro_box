49dc15f57fccc5dab762b6b18d2b8cea5ce638b1
Shtol Leonid <shtol.leonid@mail.ru>
2023-06-19 22:12:28 +0500

    start prod update

---
+++ bin/prod.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+const [webpackClientConfig, webpackServerConfig] = require('../webpack.config');
+const nodemon = require('nodemon');
+const path = require('path');
+
+const compiler = webpack([webpackServerConfig, webpackClientConfig]);
+
+compiler.run((err) => {
+  if (err) {
+    console.log(`compilation failed:`, err);
+  }
+
+  compiler.watch({}, (err) => {
+    if (err) {
+      console.log(`compilation failed:`, err);
+    }
+
+    console.log('Compilation was successful');
+  });
+
+  nodemon({
+    script: path.resolve(__dirname, '../dist/server/server.js'),
+    watch: [
+      path.resolve(__dirname, '../dist/server'),
+      path.resolve(__dirname, '../dist/client'),
+    ],
+  });
+});
