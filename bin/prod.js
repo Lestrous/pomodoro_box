2dbdfbd8165a64fabc65d9ed30e8383ca28bcaaa
Shtol Leonid <shtol.leonid@mail.ru>
2023-06-19 22:30:17 +0500

    start prod update

--- bin/prod.js
+++ bin/prod.js
@@ -20,9 +20,5 @@ compiler.run((err) => {
 
   nodemon({
     script: path.resolve(__dirname, '../dist/server/server.js'),
-    watch: [
-      path.resolve(__dirname, '../dist/server'),
-      path.resolve(__dirname, '../dist/client'),
-    ],
   });
 });
